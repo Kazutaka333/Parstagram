d829bf66dc9e13aedf388b113302b4cae4733515
Kazutaka Homma <special18fender@gmail.com>
2019-04-02 18:37:15 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Kazutaka Homma on 4/2/19.
+//  Copyright © 2019 Kazutaka Homma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
